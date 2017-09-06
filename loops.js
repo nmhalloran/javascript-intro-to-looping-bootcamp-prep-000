bce7cdd6116f7b581f5d065be91a54fb945ed866
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-06 02:42:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,8 +4,13 @@ function forLoop(array) {
   i = 0
 
   for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am " + i + " strange loop")
+    }
+    else {
       array.push("I am " + i + " strange loops")
     }
+  }
 }
 
 forLoop(weirdLoop)

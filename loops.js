54f1569b4ad13a0af9770d2e697f79a5da4b12a3
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-06 02:39:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,9 +5,8 @@ function forLoop(array) {
 
   for (var i = 0; i < 25; i++) {
       array.push("I am ${i} strange loops") [i]
-      console.log(array[i])
     }
-  return array
 }
 
 forLoop(weirdLoop)
+weirdLoop;
